39825b2f61b134a0fdc6650a8b6f480e846487d1
robnagler <github@q33.us>
2018-11-07 21:53:15 +0000

    fedora base image

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-build_image_base=radiasoft/python2
+build_image_base=radiasoft/fedora
 build_is_public=
 build_no_clean=1
 
 build_as_run_user() {
     install_repo_eval code common
-    build_run_user_home_chmod_public
 }
